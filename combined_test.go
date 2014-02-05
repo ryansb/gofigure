51ded7b780375aab00de7d5f72655f6aaf6ff84c
Ryan S. Brown <sb@ryansb.com>
2014-02-05 10:34:25 -0600

    test option precedence

--- combined_test.go
+++ combined_test.go
@@ -4,8 +4,12 @@ package gofigure
 
 import (
 	"github.com/ryansb/gofigure/conf"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	. "launchpad.net/gocheck"
+	"os"
 	"testing"
+	"time"
 )
 
 type BasicS struct{}
@@ -25,8 +29,71 @@ type MyAppSpec struct {
 
 func init() {
 	conf.Settings.Option(conf.MongoDBHosts("172.16.0.101"))
-	conf.Settings.Option(conf.FileLocations("sample/simple.json"))
+	conf.Settings.Option(conf.FileLocations("file/sample/simple.json"))
 }
 
 // TODO: make sure that config value heirarchy matches the spec
 //       env var > local file > database
+
+func (s *BasicS) TestEnvOverridesFile(c *C) {
+	origdb := conf.Settings.Option(conf.UseDB(false))
+	os.Setenv("GOFIGURE_PORT", "4321")
+	thespec := MyAppSpec{}
+	err := Process(&thespec)
+	c.Check(err, Equals, nil)
+	c.Check(thespec.Port, Equals, 4321)
+	c.Check(thespec.AlarmLoadPct, Equals, float32(0.9))
+
+	// Now turn off environment config and retry
+	origenv := conf.Settings.Option(conf.UseEnv(false))
+	thespec = MyAppSpec{}
+	err = Process(&thespec)
+	c.Check(err, Equals, nil)
+	c.Check(thespec.Port, Equals, 1234)
+	conf.Settings.Option(origenv)
+
+	conf.Settings.Option(origdb)
+}
+func (s *BasicS) TestEnvOverridesDB(c *C) {
+	origfile := conf.Settings.Option(conf.UseFile(false))
+	session, err := mgo.DialWithTimeout(conf.Settings.MongoDBHosts, 500*time.Millisecond)
+	if err != nil {
+		c.Fatalf(err.Error())
+	}
+	defer session.Close()
+	err = session.DB("gofigure").C("default").DropCollection()
+	if err != nil && err.Error() != "ns not found" {
+		c.Fatalf("Error dropping collection: %s", err.Error())
+	}
+	err = session.DB("gofigure").C("default").Create(&mgo.CollectionInfo{})
+	if err != nil {
+		c.Fatalf("Error creating collection: %s", err.Error())
+	}
+	coll := session.DB("gofigure").C("default")
+	err = coll.Insert(bson.M{
+		"Port":         1234,
+		"AlarmLoadPct": 0.9,
+		"Env":          "test",
+		"Debug":        true,
+	})
+	if err != nil {
+		c.Fatalf("Failed to insert basic config. %s", err.Error())
+	}
+
+	os.Setenv("GOFIGURE_PORT", "4321")
+	thespec := MyAppSpec{}
+	err = Process(&thespec)
+	c.Check(err, Equals, nil)
+	c.Check(thespec.Port, Equals, 4321)
+	c.Check(thespec.AlarmLoadPct, Equals, float32(0.9))
+
+	// Now try without the env
+	origenv := conf.Settings.Option(conf.UseEnv(false))
+	thespec = MyAppSpec{}
+	err = Process(&thespec)
+	c.Check(err, Equals, nil)
+	c.Check(thespec.Port, Equals, 1234)
+	conf.Settings.Option(origenv)
+
+	conf.Settings.Option(origfile)
+}
